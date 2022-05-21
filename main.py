43be277908321648957dc6218f22c05e9950e548
Mr.Samet <s.burgazoglu@gmail.com>
2022-05-21 21:33:34 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+def samet_sort(karisik_liste):
+    son_degisiklik_yeri = len(karisik_liste) - 1
+    bitis = False
+    command_count = 0
+    while not bitis:
+        j = 0
+        a = son_degisiklik_yeri
+        bitis = True
+        while j < son_degisiklik_yeri:
+            if karisik_liste[j] < karisik_liste[j + 1]:
+                karisik_liste[j], karisik_liste[j + 1] = karisik_liste[j + 1], karisik_liste[j]
+                a = j
+                bitis = False
+            command_count += 1
+            j += 1
+        son_degisiklik_yeri = a
+    return karisik_liste
+
+karisik_liste = [42, 37, 100, 150, 55, 57, 56, 25, 27, 26]
+result = samet_sort(karisik_liste)
+print(result)
